6c46d62cbfe308018651c83de5dd65e3a0f74951
kimminkum <100018239+kimminkum@users.noreply.github.com>
2025-08-19 11:54:05 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
+
+// 지웠던 스와이퍼 업데이트
 import "swiper/css";
 import "swiper/css/pagination";
 
